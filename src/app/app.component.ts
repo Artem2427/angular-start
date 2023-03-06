d86b4ce9fc41ab94b090a07470cc16e38ac3fb93
Artem Danko <danko5012@gmail.com>
2023-03-06 17:29:07 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './models/product';
+import { products as data } from './data/products';
+import { ProductsService } from './services/products.service';
+import { Observable, tap } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  title = 'Angular-app';
+  loading = false;
+  // products: IProduct[] = [];
+  products$: Observable<IProduct[]>;
+
+  constructor(private productsService: ProductsService) {}
+
+  ngOnInit(): void {
+    this.loading = true;
+    this.products$ = this.productsService
+      .getAll()
+      .pipe(tap(() => (this.loading = false)));
+    // this.productsService.getAll().subscribe((products) => {
+    //   this.products = products;
+    //   this.loading = false;
+    // });
+  }
+}
